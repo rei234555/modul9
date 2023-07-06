b6118541897c86085f54e280f2c7587141dccce3
rei234555 <reinantosaputra@gmail.com>
2023-07-07 00:03:22 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,134 @@
+<?php
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Auth\LoginController;
+use Illuminate\Support\Facades\Storage;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('home', [HomeController::class, 'index'])->name('home')->middleware('auth');
+
+Route::get('employees', [HomeController::class, 'index'])->name('employees.index')->middleware('auth');
+Route::get('profile', [HomeController::class, 'index'])->name('profile')->middleware('auth');
+
+Route::get('profile', ProfileController::class)->name('profile')->middleware('auth');
+
+Route::resource('employees', EmployeeController::class)->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
+
+Route::post('login', [LoginController::class, 'authenticate']);
+
+
+// Meletakkan File pada Local Disk
+
+Route::get('/local-disk', function() {
+    Storage::disk('local')->put('local-example.txt', 'This is local example content');
+    return asset('storage/local-example.txt');
+});
+
+
+// Meletakkan File pada Public Disk
+
+Route::get('/public-disk', function() {
+    Storage::disk('public')->put('public-example.txt', 'This is public example content');
+    return asset('storage/public-example.txt');
+});
+
+// Menampilkan isi File Local
+
+Route::get('/retrieve-local-file', function() {
+    if (Storage::disk('local')->exists('local-example.txt')) {
+        $contents = Storage::disk('local')->get('local-example.txt');
+    } else {
+        $contents = 'File does not exist';
+    }
+
+    return $contents;
+});
+
+// Menampilkan isi File Public
+
+Route::get('/retrieve-public-file', function() {
+    if (Storage::disk('public')->exists('public-example.txt')) {
+        $contents = Storage::disk('public')->get('public-example.txt');
+    } else {
+        $contents = 'File does not exist';
+    }
+
+    return $contents;
+});
+
+// Mendownload file local
+
+Route::get('/download-local-file', function() {
+    return Storage::download('local-example.txt', 'local file');
+});
+
+// Mendownload file public
+
+Route::get('/download-public-file', function() {
+    return Storage::download('public/public-example.txt', 'public file');
+});
+
+// Menampilkan URL, Path dan Size dari File
+
+Route::get('/file-url', function() {
+    // Just prepend "/storage" to the given path and return a relative URL
+    $url = Storage::url('local-example.txt');
+    return $url;
+});
+
+Route::get('/file-size', function() {
+    $size = Storage::size('local-example.txt');
+    return $size;
+});
+
+Route::get('/file-path', function() {
+    $path = Storage::path('local-example.txt');
+    return $path;
+});
+
+// Menyimpan file via form
+
+Route::get('/upload-example', function() {
+    return view('upload_example');
+});
+
+Route::post('/upload-example', function(Request $request) {
+    $path = $request->file('avatar')->store('public');
+    return $path;
+})->name('upload-example');
+
+// Menghapus file pada file storage
+
+Route::get('/delete-local-file', function(Request $request) {
+    Storage::disk('local')->delete('local-example.txt');
+    return 'Deleted';
+});
+
+Route::get('/delete-public-file', function(Request $request) {
+    Storage::disk('public')->delete('public-example.txt');
+    return 'Deleted';
+});
+
+Route::get('download-file/{employeeId}', [EmployeeController::class, 'downloadFile'])->name('employees.downloadFile');
